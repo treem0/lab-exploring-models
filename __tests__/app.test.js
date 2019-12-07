c8d0b9f941d89b3270455d8796402a93d9dd4f64
Travis Molter <travismolter@gmail.com>
2019-12-06 16:58:34 -0800

    working on the get car by ID route with no luck

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,6 +18,18 @@ describe('application routes', () => {
         });
       });
   });
+  it('has a route that gets a car by ID', () => {
+    return request(app)
+      .get('/car/:id')
+      .then(res => {
+        expect(res.params).toContainEqual({
+          __v: expect.any(Number),
+          _id: '5deadd97db36e77d24ff4189',
+          name: 'Tesla Cyber Truck',
+          horsepower: 500, 
+        });
+      });
+  });
   
   it('has a /cars post route', () => {
     return request(app)
@@ -35,21 +47,21 @@ describe('application routes', () => {
         });
       });
   });
-  it('has a /car PUT route', () => {
-    return request(app)
-      .put('/car/:id')
-      .send({
-        name: 'Tesla Model S',
-        horsepower: 300
-      })
-      .then(res => {
-        expect(res.body).toEqual({
-          __v: expect.any(Number),
-          _id: expect.any(String),
-          name: 'Tesla Model S',
-          horsepower: 300,
-        });
-      });
-  });
+  // it('has a /car PUT route', () => {
+  //   return request(app)
+  //     .put('/cars/:id')
+  //     .send({
+  //       name: 'Tesla Model S',
+  //       horsepower: 300
+  //     })
+  //     .then(res => {
+  //       expect(res.params).toEqual({
+  //         __v: expect.any(Number),
+  //         _id: expect.any(String),
+  //         name: 'Tesla Model S',
+  //         horsepower: 300,
+  //       });
+  //     });
+  // });
 });
   
