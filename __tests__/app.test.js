b98a632b99c52f5153fc38cf576d77d3fa3c4d90
Travis Molter <travismolter@gmail.com>
2019-12-16 21:56:02 -0800

    got my routes and tests working. should be good to go

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,30 +1,49 @@
 require('dotenv').config();
-require('../lib/utils/connect')();
+const connect = require('../lib/utils/connect');
 const request = require('supertest');
 const app = require('../lib/app');
+const mongoose = require('mongoose'); 
 const Car = require('../lib/models/Car');
 
-
 describe('application routes', () => {
-  it('has a route that gets all the cars', () => {
+  beforeAll(done => {
+    done();
+  });
+  afterAll(done => {
+    mongoose.connection.close();
+    done();
+  });
+  
+  beforeAll(() =>{
+    connect();
+  });
+
+  beforeEach(() =>{
+    return mongoose.connection.dropDatabase();
+  });
+  afterAll(() =>{
+    return mongoose.connection.close();
+  });
+
+  it('has a home route', () =>{
     return request(app)
       .get('/')
-      .then(res => {
-        expect(res.body).toContainEqual({ 
-          __v: expect.any(Number),
-          _id: expect.any(String),
-          name: 'Tesla Cyber Truck',
-          horsepower: 500, 
-        });
-      });
+      .then(res =>[
+        expect(res.body).toEqual({ text: 'all the cars' })
+      ]);
   });
-  it('has a route that gets a car by ID', () => {
+
+  it('has a route that gets a car by ID', async() => {
+    const teslaCar = await Car.create({
+      name: 'Tesla Model S',
+      horsepower: 300
+    });
     return request(app)
-      .get('/car/:id')
+      .get(`/cars/${teslaCar._id}`)
       .then(res => {
         expect(res.body).toEqual({
           __v: 0,
-          _id: '5deadd97db36e77d24ff4189',
+          _id: expect.any(String),
           name: 'Tesla Model S',
           horsepower: 300, 
         });
@@ -48,19 +67,18 @@ describe('application routes', () => {
       });
   });
   it('has a /car PUT route', async() => {
-    const car = await Car.create({
-      name: 'tesla',
-      horsepower: 400
+    const teslaCar = await Car.create({
+      name: 'Tesla Cyber Truck',
+      horsepower: 500
     });
-
     return request(app)
-      .put(`/cars/${car._id}`)
+      .put(`/update/${teslaCar._id}`)
       .send({
         name: 'Tesla Cyber Truck',
-        horsepower: 500,
+        horsepower: 500
       })
       .then(res => {
-        expect(res.params).toEqual({
+        expect(res.body).toEqual({
           _id: expect.any(String),
           name: 'Tesla Cyber Truck',
           horsepower: 500,
@@ -68,5 +86,23 @@ describe('application routes', () => {
         });
       });
   });
+
+  it('has a /cars by ID delete route', async() =>{
+    const teslaCar = await Car.create({
+      name: 'Tesla Model S',
+      horsepower: 300
+    });
+    return request(app)
+      .delete(`/cars/${teslaCar._id}`)
+      .then(res =>{
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Tesla Model S',
+          horsepower: 300,
+          __v: expect.any(Number) 
+        });
+      });
+  });
 });
+
   
